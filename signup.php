0b3bab16acae39812cda7c3b762ecfa85f498ae6
marcooo <129814665+izmarcoe@users.noreply.github.com>
2024-08-24 22:38:24 +0800

    login/signup

---
+++ signup.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up</title>
+    <link rel="stylesheet" href="dist/output.css">
+    <link rel="stylesheet" href="src/input.css">
+</head>
+<body>
+    <h1>SIGN UP FORM</h1>
+</body>
+</html>
